cd13a6012a322b33dbdbc7d7465c520d6295851c
GarvG <67434601+GarvG@users.noreply.github.com>
2021-11-30 23:31:35 +0530

    mark14

---
+++ app.js
@@ -0,0 +1,38 @@
+var initialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var Output = document.querySelector("#output");
+
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stockQuantity.value);
+  var curr = Number(currentPrice.value);
+
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+
+    message(
+      `Its in the loss which is ${loss} and the percentage is ${lossPercentage} %`
+    );
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+
+    message(
+      `Congo!!  the profit is ${profit} and the percentage is ${profitPercentage} %`
+    );
+  } else {
+    message(`There is No Profit No Loss!!`);
+  }
+}
+
+function message(msg) {
+  Output.innerText = msg;
+}
+submitBtn.addEventListener("click", submitHandler);
